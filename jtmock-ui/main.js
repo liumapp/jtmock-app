7103f6098589ffc74a191e9099835c9956081274
liumapp <liumapp.com@gmail.com>
2023-09-26 15:57:09 +0800

    update ui package

---
+++ jtmock-ui/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+    globalShortcut.register('Alt+Command+J', () => {
+        win.webContents.openDevTools({mode:'detach'}) //开启开发者工具
+    })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+const template = [
+    {
+        label: 'Edit',
+        submenu: [
+            {role: 'cut'},
+            {role: 'copy'},
+            {role: 'paste'},
+            {role: 'about'}
+        ]
+    }
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+
