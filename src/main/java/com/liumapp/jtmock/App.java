22820c62276a0969bebfb6426b48c8536bcbcb75
liumapp <liumapp.com@gmail.com>
2023-09-27 15:20:25 +0800

    update jtmock main pannel

--- src/main/java/com/liumapp/jtmock/App.java
+++ src/main/java/com/liumapp/jtmock/App.java
@@ -1,13 +1,27 @@
 package com.liumapp.jtmock;
 
+import com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme;
+
+import javax.swing.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static JFrame mainFrame = new JFrame(String.valueOf(FlatOneDarkIJTheme.setup()));
+
+    public static void main(String[] args) {
+
+        mainFrame.setTitle("JTMock");
+        mainFrame.setResizable(true);
+//        MainPanel mainPanel = new MainPanel();
+//        mainFrame.setContentPane(mainPanel.panel);
+        mainFrame.pack();
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
 }
---
+++ src/main/java/com/liumapp/jtmock/view/MainPanel.java
@@ -0,0 +1,12 @@
+package com.liumapp.jtmock.view;
+
+
+/**
+ * @file MainPanel.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2023/9/27 15:18
+ */
+public class MainPanel {
+}
