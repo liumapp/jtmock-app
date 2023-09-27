636803f1534e1071100be3867a8ad3a153eeb934
liumapp <liumapp.com@gmail.com>
2023-09-27 16:33:23 +0800

    update main pannel

--- src/main/java/com/liumapp/jtmock/view/MainPanel.java
+++ src/main/java/com/liumapp/jtmock/view/MainPanel.java
@@ -7,11 +7,13 @@ package com.liumapp.jtmock.view;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import javax.swing.border.*;
 
 /**
  * @author liumapp
  */
 public class MainPanel {
+
     public MainPanel() {
         initComponents();
     }
@@ -20,28 +22,73 @@ public class MainPanel {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - unknown
         panel = new JPanel();
+        subPanel = new JPanel();
+        configBtn = new JButton();
+        title = new JLabel();
+        mockBtn = new JButton();
 
         //======== panel ========
         {
             panel.setPreferredSize(new Dimension(800, 600));
-            panel.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (
-            new javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion"
-            , javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM
-            , new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 )
-            , java. awt. Color. red) ,panel. getBorder( )) ); panel. addPropertyChangeListener (
-            new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e
-            ) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( )
-            ; }} );
+            panel.setMinimumSize(new Dimension(866, 300));
+            panel.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing.
+            border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border. TitledBorder. CENTER
+            , javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font
+            .BOLD ,12 ), java. awt. Color. red) ,panel. getBorder( )) ); panel. addPropertyChangeListener (
+            new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r"
+            .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+
+            //======== subPanel ========
+            {
+                subPanel.setBorder(new LineBorder(Color.white));
+                subPanel.setForeground(Color.pink);
+
+                GroupLayout subPanelLayout = new GroupLayout(subPanel);
+                subPanel.setLayout(subPanelLayout);
+                subPanelLayout.setHorizontalGroup(
+                    subPanelLayout.createParallelGroup()
+                        .addGap(0, 648, Short.MAX_VALUE)
+                );
+                subPanelLayout.setVerticalGroup(
+                    subPanelLayout.createParallelGroup()
+                        .addGap(0, 528, Short.MAX_VALUE)
+                );
+            }
+
+            //---- configBtn ----
+            configBtn.setText("\u901a\u8baf\u914d\u7f6e");
+
+            //---- title ----
+            title.setText("JTMock");
+            title.setFont(title.getFont().deriveFont(title.getFont().getSize() + 15f));
+
+            //---- mockBtn ----
+            mockBtn.setText("\u6a21\u62df\u62a5\u6587");
 
             GroupLayout panelLayout = new GroupLayout(panel);
             panel.setLayout(panelLayout);
             panelLayout.setHorizontalGroup(
                 panelLayout.createParallelGroup()
-                    .addGap(0, 645, Short.MAX_VALUE)
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(panelLayout.createParallelGroup()
+                            .addComponent(title)
+                            .addComponent(configBtn, GroupLayout.PREFERRED_SIZE, 198, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(mockBtn, GroupLayout.PREFERRED_SIZE, 198, GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(subPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             );
             panelLayout.setVerticalGroup(
                 panelLayout.createParallelGroup()
-                    .addGap(0, 415, Short.MAX_VALUE)
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(title)
+                        .addGap(52, 52, 52)
+                        .addComponent(configBtn, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(mockBtn, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(298, Short.MAX_VALUE))
+                    .addComponent(subPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             );
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
@@ -50,5 +97,9 @@ public class MainPanel {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - unknown
     private JPanel panel;
+    private JPanel subPanel;
+    private JButton configBtn;
+    private JLabel title;
+    private JButton mockBtn;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
