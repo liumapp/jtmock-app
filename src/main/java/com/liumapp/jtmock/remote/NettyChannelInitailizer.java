0d49672c915c1b1df91d1788df5569d8a192891d
liumapp <liumapp.com@gmail.com>
2023-10-07 17:01:00 +0800

    update netty in bound handler

---
+++ src/main/java/com/liumapp/jtmock/remote/NettyChannelInitailizer.java
@@ -0,0 +1,20 @@
+package com.liumapp.jtmock.remote;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @file NettyChannelInitailizer.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2023/10/7 16:45
+ */
+public class NettyChannelInitailizer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+    }
+
+}
---
+++ src/main/java/com/liumapp/jtmock/remote/NettyInBoundHandler.java
@@ -0,0 +1,25 @@
+package com.liumapp.jtmock.remote;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @file NettyInBoundHandler.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2023/10/7 16:59
+ */
+public class NettyInBoundHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+}
